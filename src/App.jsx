82089fd1fd21df27ef7cea4ec17958374f3fe3bc
jorsema2 <jorsema2@gmail.com>
2024-10-07 10:20:53 +0200

    Moved Strict Mode up to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import React from "react";
 import "./App.css";
 import UsersFeedPage from "./views/UsersFeedPage";
 
 function App() {
   return (
     <>
-      <React.StrictMode>
-        <UsersFeedPage />
-      </React.StrictMode>
+      <UsersFeedPage />
     </>
   );
 }
